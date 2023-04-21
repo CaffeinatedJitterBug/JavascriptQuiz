d559666d74a7d096b36731d25b371b212684124a
CaffeinatedJitterbug <itwallaby@gmail.com>
2023-04-21 18:13:21 -0400

    Moved questions property out of qna and made it a stand alone array. Changed the name of qna object to answers. Edited nextQuestion function to use values stored in answer object for the textContent of the buttons.

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ const getDiv = document.querySelector("div");
 const header = document.createElement("h1");
 const questionNumber = 0;
 
-const qna = {
-    questions: ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"],
-    answers1: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"]
+const questions = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"];
+const answers = {
+    question1: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"]
 }
+
 
 function nextQuestion() {
     while (getDiv.firstChild) {
         getDiv.removeChild(getDiv.lastChild);
     }
-    header.textContent = qna.questions[questionNumber];
+    header.textContent = questions[questionNumber];
     getDiv.appendChild(header);
+    const answerList = Object.values(answers)[questionNumber];
     for (x=0; x<4; x++) {
         const button = document.createElement("button");
-        button.textContent = qna.answers1[x];
+        button.textContent = answerList[x];
         getDiv.appendChild(button);
     }
 }
