0b13e1a243a9614ff6a4f732db9fa5c89807e7e9
CaffeinatedJitterbug <itwallaby@gmail.com>
2023-04-21 17:46:01 -0400

    Added an object that holds placeholder questions and answers. Edited the nextQuestion function to use values stored in the qna object for the textContent of the header and buttons.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const startButton = document.getElementById("start");
 const getDiv = document.querySelector("div");
 const header = document.createElement("h1");
+const questionNumber = 0;
 
-const test = ["test 1", "test 2", "test 3", "test 4"]
+const qna = {
+    questions: ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"],
+    answers1: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"]
+}
 
 function nextQuestion() {
     while (getDiv.firstChild) {
         getDiv.removeChild(getDiv.lastChild);
     }
-    header.textContent = "Test question";
+    header.textContent = qna.questions[questionNumber];
     getDiv.appendChild(header);
     for (x=0; x<4; x++) {
         const button = document.createElement("button");
-        button.textContent = test[x];
+        button.textContent = qna.answers1[x];
         getDiv.appendChild(button);
     }
 }
