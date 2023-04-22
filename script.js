73c6c76579cb6f06ecc266faf4e4296031e3f859
CaffeinatedJitterbug <itwallaby@gmail.com>
2023-04-22 15:03:30 -0400

    Added the variables rightAnswers and wrongAnswers. Built upon the if statements in the getDiv event listener to detect if the button being clicked is an answer button and increment either rightAnswers or wrongAnswers based upon which button is clicked. Added on to the nextQuestion function so that questionNumber is incremented whenever it finishes running.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // Gets the div element, creates a new h1 element, and tracks progress through quiz
 const getDiv = document.querySelector("div");
 const header = document.createElement("h1");
-const questionNumber = 0;
+let questionNumber = 0;
+let rightAnswers = 0;
+let wrongAnswers = 0;
 
 // Stores questions and answers
 const questions = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"];
 const answers = {
-    question1: ["Answer 1", "Answer 2", "Answer 3", "Answer 4", 0]
+    question1: ["Answer 1", "Answer 2", "Answer 3", "Answer 4", 0],
+    question2: ["Answer 5", "Answer 6", "Answer 7", "Answer 8", 1]
 }
 
 // Clears visible elements and generates a new h1 element and four buttons. Sets textContent based off of values stored in questions array and answers object
@@ -27,6 +30,7 @@ function nextQuestion() {
         }
         getDiv.appendChild(button);
     }
+    questionNumber++;
 }
 
 // Checks to make sure element being clicked is a button and runs nextQuestion function if the button labeled "start" is clicked
@@ -34,6 +38,13 @@ getDiv.addEventListener("click", function(event) {
     const element = event.target;
     if (element.matches("button")) {
         if (element.className === "start") {
+            nextQuestion();
+        } else if (element.getAttribute("data-name") === "correct") {
+            rightAnswers++;
+            nextQuestion();
+            console.log(rightAnswers);
+        } else {
+            wrongAnswers++;
             nextQuestion();
         }
     }
